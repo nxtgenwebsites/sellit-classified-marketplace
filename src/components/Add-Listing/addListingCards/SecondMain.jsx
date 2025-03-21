c769ec3a7c748388b162a7936c8dc57d41781aa1
Muhammad Ahad <ahad97140@gmail.com>
2025-03-21 15:27:19 +0500

    Changes made 2

--- src/components/Add-Listing/addListingCards/SecondMain.jsx
+++ src/components/Add-Listing/addListingCards/SecondMain.jsx
@@ -143,7 +143,6 @@ export default function SecondMain() {
               value={IsChanged.phone}
               name="phone"
               onChange={handleInp}
-              name="Temperory Contact Number"
             />
             <br />
             <p className="mx-auto description-para">
