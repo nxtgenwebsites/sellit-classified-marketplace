58e80570b00a70d93a814c805bd981a174a59313
shahbazansari8199@gmail.com <shahbazansari8199@gmail.com>
2025-07-19 18:39:32 +0500

    working

--- src/components/Home/Marquee.jsx
+++ src/components/Home/Marquee.jsx
@@ -10,7 +10,7 @@ import PartnersCards from "./PartnersCards";
 const Marquee = () => {
 const data = [
   {
-    name: "Pakwheel",
+    name: "Pakwheels",
     logo: "https://play-lh.googleusercontent.com/Q_3vIq94Bs2_QYFiFw4vQhobVioxzaKEq-XDuW0OEugD--iGQNLYktO2PgEcAED53v0",
     profileLink: "https://www.pakwheels.com/",
     location: "Islamabad",
